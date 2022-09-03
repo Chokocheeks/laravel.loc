b3ac0413d73fa3d967e57ec9473992b0a002bab0
Vi <anim.nika@gmail.com>
2022-09-03 23:11:09 +0300

    add product

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\Admin\MyController;
+use App\Http\Controllers\Admin\product\ProductController;
 use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 
@@ -29,10 +30,23 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::prefix('/admin')->group(function(){
+    // Categories
     Route::get('/', [\App\Http\Controllers\Admin\MyController::class, 'index'])
     ->name('categories.index');
+    Route::get('/categories', [\App\Http\Controllers\Admin\CategoryController::class, 'index']);
+    Route::get('/categories/create', [CategoryController::class, 'create']);
     
+    Route::post('/categories/create', [CategoryController::class, 'store']);
+    
+    Route::get('/categories/{category}/edit', [CategoryController::class, 'edit']);
+    
+    Route::put('/categories/{category}/update', [CategoryController::class, 'update']);
+    
+    Route::delete('/categories/{category}/delete', [CategoryController::class, 'delete']);
 
     Route::resource('categories', CategoryController::class)
     ->except(['show']);
+
+    // Product
+    // Route::get('/product', [ProductController::class, 'index'])->name('product.index');
 });
