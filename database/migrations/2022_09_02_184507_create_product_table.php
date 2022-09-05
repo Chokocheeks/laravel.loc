88246ae8fb76585385806dff4c16e1e45251f5a9
Vi <anim.nika@gmail.com>
2022-09-05 18:59:11 +0300

    add int

--- database/migrations/2022_09_02_184507_create_product_table.php
+++ database/migrations/2022_09_02_184507_create_product_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->string('description', 255);
             $table->integer('category_id');
             $table->string('image');
-            $table->float('price', );
+            $table->integers('price', );
             $table->boolean('active');
             $table->timestamps();
         });
