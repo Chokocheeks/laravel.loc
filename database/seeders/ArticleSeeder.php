41f2fddf7f4fa1c04950d237339130370423fe7c
Vi <anim.nika@gmail.com>
2022-09-12 08:49:50 +0300

    add article

--- database/seeders/ArticleSeeder.php
+++ database/seeders/ArticleSeeder.php
@@ -2,11 +2,11 @@
 
 namespace Database\Seeders;
 
-use App\Models\Product;
+use App\Models\Article;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class ProductSeeder extends Seeder
+class ArticleSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -15,6 +15,6 @@ class ProductSeeder extends Seeder
      */
     public function run()
     {
-        Product::factory(10)->create();
+        Article::factory(10)->create();
     }
 }
